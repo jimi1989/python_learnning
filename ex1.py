18259ed6766a69b442527087f2832b95ca52a00c
jimi1989 <jinjianghao@kaishengit.com>
2018-01-10 15:24:20 +0800

    变量 字符串

---
+++ ex1.py
@@ -0,0 +1,12 @@
+print "hello world!"
+print 1 != 2
+cars = 100
+print "there are ", cars, "cars."
+my_eyes = 'blue'
+my_hair = 'black'
+print "he's got %s eyes and %s hair"%(my_eyes,my_hair)
+my_age = 10
+print "he is %d ages years old" %my_age
+w = "hello"
+y = "world"
+print w + y
